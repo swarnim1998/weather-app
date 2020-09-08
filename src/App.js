3072b76243910b002662cc7ac1fd3292cbb16cc2
Swarnim Mukati <swarnim.Mukati@mountblue.tech>
2020-09-08 12:12:46 +0530

    added SearchBar.js component and make the api call to get data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Search from "./components/SearchBar";
+import Weather from "./components/Weather";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      temp: "",
+      weather: "",
+      city: "",
+      check: false,
+      hasError: false,
+    };
+  }
+
+  callApi(city, country) {
+    const API_KEY = "0c2311c83e5c6bb1cc4ae0483498117c";
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          temp: res.main.temp,
+          weather: res.weather[0].main,
+          city: city,
+          check: true,
+        });
+      });
+  }
+
+  getWeather = (event) => {
+    event.preventDefault();
+    const city = event.target[0].value;
+    const country = event.target[1].value;
+    this.callApi(city, country);
+  };
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    console.log(error);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        <Search getWeather={this.getWeather} />
+      </div>
+    );
+  }
 }
 
 export default App;
