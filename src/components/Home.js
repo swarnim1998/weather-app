77c2bbee430eba84c9e7eb41a0060702af12e6c3
Swarnim Mukati <swarnim.Mukati@mountblue.tech>
2020-09-09 13:54:38 +0530

    modify home.js component and put search and weather component inside this

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,18 @@ import Search from "./SearchBar";
 import Weather from "./Weather";
 
 function Home(props) {
-  return <div></div>;
+  return (
+    <div>
+      <Search getWeather={props.getWeather} className="search" />
+      {props.check ? (
+        <Weather temp={props.temp} weather={props.weather} city={props.City} />
+      ) : (
+        <div className="intro">
+          Get the whether data of any city in the world
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
